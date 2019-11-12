38dac0ea284bf6bc0255e23a885dc5e03a822a33
leehwasub <hwasub1115@naver.com>
2019-11-12 15:36:26 +0900

    #5 Coding the UI part two - Input and To Do Components

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import ToDo from "./ToDo";
 
 const {height, width} = Dimensions.get("window");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>Kawai To Do</Text>
-      <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"New To Do"}></TextInput>
+export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
+
+  render(){
+    const {newToDo} = this.setState;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Kawai To Do</Text>
+        <View style={styles.card}>
+          <TextInput 
+            style={styles.input} 
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._crontolNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}>
+          </TextInput>
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
+  _crontolNewToDo = text =>{
+    this.setState({
+      newToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -48,5 +72,11 @@ const styles = StyleSheet.create({
         elevation:3
       }
     })
+  },
+  input:{
+    padding:20,
+    borderBottomColor:"#bbb",
+    borderBottomWidth: 1,
+    fontSize:25
   }
 });
